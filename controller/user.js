556416365f1753ac4c6cfb76b6135bdb54343e35
Girish Chaudhari <64349045+girish-chaudhari@users.noreply.github.com>
2021-09-24 20:59:00 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -3,45 +3,46 @@ const responseHandler = require("../config");
 const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res, next) => {
- try{
-  const newUser = user(req.body);
-  const savedUser = await newUser.save();
-  responseHandler(res, 200, "user registered successfully ", savedUser);
- }catch(error){
-  responseHandler(res, 500, "something went wrong", {}) 
- }
+  try {
+    const newUser = user(req.body);
+    const savedUser = await newUser.save();
+    return responseHandler(
+      res,
+      200,
+      "user registered successfully ",
+      savedUser
+    );
+  } catch (error) {
+    return responseHandler(res, 500, "something went wrong", {});
+  }
 };
 
 exports.signin = async (req, res, next) => {
-  try{
-    const authUser = await user.findOne({email: req.body.email})  
-    if(authUser){
-      if(req.body.password === authUser.password){
+  try {
+    const authUser = await user.findOne({ email: req.body.email });
+    if (authUser) {
+      if (req.body.password === authUser.password) {
         const token = jwt.sign(
-          {userId :authUser._id},
+          { userId: authUser._id },
           process.env.JWT_SECRET,
-          {expiresIn : '1d'}
-        )
-        return  responseHandler(res, 200, "logged in successfully", {token})
-      }else{
-        responseHandler(res, 401, "invalid credential", {})
+          { expiresIn: "1d" }
+        );
+        return responseHandler(res, 200, "logged in successfully", { token });
+      } else {
+        responseHandler(res, 401, "invalid credential", {});
       }
-
-    }else{
-      return responseHandler(res, 404, "user not found", {})
-      
+    } else {
+      return responseHandler(res, 404, "user not found", {});
     }
+  } catch (err) {
+    return responseHandler(res, 500, "something went wrong", {});
   }
-  catch(err){
-    return responseHandler(res, 500, "something went wrong", {})
-  }
-  
 };
 
 exports.contact = (req, res, next) => {
   try {
     responseHandler(res, 200, "success ", { message: "success" });
   } catch (err) {
-    responseHandler(res, 500, "something went wrong ", {});
+    responseHandler(res, 500, "something went wrong ", {}); ////
   }
 };
